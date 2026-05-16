dcdeb83d0b9d8cd3f6781c6dfd1863bd796d7084
At <atbspb@qq.com>
2026-05-17 03:36:24 +0800

    版权信息添加 Powered by Vue 和 Netlify 链接

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,6 +1,7 @@
 <template>
   <footer>
-    At - Home Page &copy; {{ currentYear }}
+    At - Home Page &copy; {{ currentYear }} . //
+    Powered by <a href="https://vuejs.org/" target="_blank">Vue</a> &amp; <a href="https://www.netlify.com" target="_blank">Netlify</a>
   </footer>
 </template>
 
