ede8632d791b5923ca667e626865e73c384f5edf
At <atbspb@qq.com>
2026-05-17 14:09:07 +0800

    添加 MIT 许可证 - LICENSE 文件、版权信息及源码头部声明

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,6 +1,9 @@
+Copyright (c) {{ currentYear }} ATBSPB. Licensed under the MIT License.
+
 <template>
   <footer>
-    At - Home Page &copy; {{ currentYear }} . //
+    Copyright &copy; {{ currentYear }} ATBSPB. All rights reserved.
+    <br />
     Powered by <a href="https://www.netlify.com" target="_blank">Netlify</a> &amp; <a href="https://zyyo.net/" target="_blank">zyyo</a>
   </footer>
 </template>
